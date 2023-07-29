dc695ba0f99cbd8d1e632c03e38e22dfd9c7d284
rahulholkar16 <usefulltech1819@gmail.com>
2023-07-29 23:53:27 +0530

    Update Filter Component

--- src/component/filter/Filter.jsx
+++ src/component/filter/Filter.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Filter.scss'
+import ContentWrapper from '../contentWrapper/ContentWrapper'
 
 const Filter = () => {
   return (
-    <div>Filter</div>
+    <ContentWrapper>
+      <div className='filter-container'>
+        <li className='country-filter list'>
+            <a>Country</a>
+            <div className='sub-menu-1'>
+                <ul>
+                    <li className='list'>INDIA</li>
+                    <li className='list'>USA</li>
+                    <li className='list'>RUSSIA</li>
+                    <li className='list'>CHAINA</li>
+                </ul>
+            </div>
+        </li>
+      </div>
+    </ContentWrapper>
+    
   )
 }
 
--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../../hook/useFetch'
 import Carousel from '../../component/carousel/Carousel'
+import Filter from '../../component/filter/Filter'
 
 
 const Explore = () => {
@@ -10,6 +11,7 @@ const Explore = () => {
 
     return (
         <>
+            <Filter />
             <Carousel data={data?.articles} loading={loading}/>
         </>
     )
