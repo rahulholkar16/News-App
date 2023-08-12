05c7ade447c89edb71fc0b62d93303d1c16484e0
rahulholkar16 <usefulltech1819@gmail.com>
2023-08-13 02:05:04 +0530

    fix file

--- utils/api.js
+++ utils/api.js
@@ -6,8 +6,6 @@ export const fetchDataFromapi = async (Url) => {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + import.meta.env.VITE_APP_API_KEY,
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
   };
 
 // Set the request configuration
@@ -15,7 +13,6 @@ export const fetchDataFromapi = async (Url) => {
     url: 'https://newsapi.org/v2/' + Url,
     method: 'GET',
     headers: headers,
-    withCredentials: true
     // You can also set other properties like data, params, etc. based on your needs
   };
 
