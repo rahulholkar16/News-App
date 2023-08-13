a2d75c65b4acd0e2be96b4956c62fd6c57f1aa16
rahulholkar16 <usefulltech1819@gmail.com>
2023-08-13 22:01:28 +0530

    Fix api.js file

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const fetchDataFromApi = async (url) => {
+  const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const headers = {
     
     'Authorization': `Bearer ${import.meta.env.VITE_APP_API_KEY}`,
@@ -14,7 +15,7 @@ export const fetchDataFromApi = async (url) => {
   };
 
   try {
-    const { data } = await axios(config);
+    const { data } = await axios.get(`https://newsapi.org/v2/${url}`, {headers});
     return data;
   } catch (err) {
     console.error(err); // Using console.error for errors
