746e3b4fcd6399221c0feaaa32efb67982cadbff
rahulholkar16 <usefulltech1819@gmail.com>
2023-08-14 00:08:22 +0530

    Fix api.js file

--- utils/api.js
+++ utils/api.js
@@ -4,6 +4,7 @@ export const fetchDataFromApi = async (url) => {
   const api = 'c4e5c729efd84a6f8f726d383f2024a5';
   const headers = {
     'X-Api-Key': api,
+    'Access-Control-Allow-Origin': '*'
   };
 
   const config = {
