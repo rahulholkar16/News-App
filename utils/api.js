ac81b818d7a772e12145a95957ca0b59748f512b
rahulholkar16 <usefulltech1819@gmail.com>
2023-08-14 21:16:48 +0530

    Fix api.js file

--- utils/api.js
+++ utils/api.js
@@ -2,22 +2,24 @@ import axios from 'axios';
 
 export const fetchDataFromApi = async (url) => {
   const api = 'c4e5c729efd84a6f8f726d383f2024a5';
-  const headers = {
-    'X-Api-Key': api,
-  };
+  // const headers = {
+  //   'X-Api-Key': api,
+  // };
 
-  const config = {
-    url: `https://newsapi.org/v2/${url}`,
+  const options = {
     method: 'GET',
-    headers,
-   // Object shorthand
+    url: `https://newsapi.org/v2/${url}`,
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + api
+    }
   };
 
   try {
-    const { data } = await axios(config);
+    const { data } = await axios.request(options);
     return data;
   } catch (err) {
-    console.error(err); // Using console.error for errors
-    throw err; // Rethrow the error to handle it further up the call stack
+    console.error(err); 
+    throw err; 
   }
 };
