c4ba15f7c78fabfc38d24c66a9237202a2317e82
rahulholkar16 <usefulltech1819@gmail.com>
2023-08-14 22:19:12 +0530

    Fix api.js file

--- utils/api.js
+++ utils/api.js
@@ -1,17 +1,12 @@
 import axios from 'axios';
 
 export const fetchDataFromApi = async (url) => {
-  const api = 'c4e5c729efd84a6f8f726d383f2024a5';
-  // const headers = {
-  //   'X-Api-Key': api,
-  // };
-
   const options = {
     method: 'GET',
     url: `https://newsapi.org/v2/${url}`,
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer ' + api
+      Authorization: 'Bearer ' + import.meta.env.VITE_APP_API_KEY
     }
   };
 
